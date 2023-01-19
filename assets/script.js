8d8e9d5b1c5fa74ab82ca0fb19e170f64caab50f
elkirko <kirkhagglund@gmail.com>
2023-01-19 17:17:00 -0600

    assigned values

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,10 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
+// Assignment Code //Done
+let generateBtn = document.querySelector("#generate");
+let lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+let numberList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let specialChar = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "<", ">", ":", ";", "?"];
+let charLength = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -11,8 +15,8 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
+//generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); 
 
 /*What do I want to happen here? 
 First interaction is the click generate password button
